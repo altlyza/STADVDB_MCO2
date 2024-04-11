14a246523379717a7ec6cc8549dfe3dc77f7a25b
wyz <alyza_reynado@dlsu.edu.ph>
2024-04-11 22:20:39 +0800

    working connection

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
-const express = require('express');
+//const express = require('express');
+
+import express from 'express';// Your application logic here
 const app = express();
 const port = 3000;
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+//import { Appointments, Node1Appointments } from './DBConn.js';
+//comment out which node you dont need
+import { Node1 } from './dbconnection.js';
+import { Node2 } from './dbconnection.js';
+import { Node3 } from './dbconnection.js';
+
+async function initializeDB(){
+
+    async function testConnection(sequelizeInstance, nodeName) {
+        try {
+          await sequelizeInstance.authenticate();
+          console.log(`Connection has been established successfully to ${nodeName}.`);
+        } catch (error) {
+          console.error(`Unable to connect to the ${nodeName}:`, error);
+        }
+    }
+    
+    //comment out the connection to which node you dont need
+    //testConnection(localConnection, 'Local Node');
+    testConnection(Node1, 'Node1');
+    testConnection(Node2, 'Node2');
+    testConnection(Node3, 'Node3');
+
+
+    //comment out the connection to which node you dont need
+    //await localConnection.sync();
+    await Node1.sync();
+    await Node2.sync();
+    await Node3.sync();
+}
+
+initializeDB();
 
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
